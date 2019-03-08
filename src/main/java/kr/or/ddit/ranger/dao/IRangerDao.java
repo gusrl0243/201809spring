135cae0bed51fdefcade3c32191b938471453f54
KHK <gusrl56@gmail.com>
2019-03-08 15:59:23 +0900

    spring aop /  선언적 트랜잭션 / spring -mybatis 연동 / 테스트 데이터 초기화
    LoggingAdvice
    1.beforeMethod
    2.afterMethod
    3.aroundMethod(ProceddingJoinPoint)
    
    aop를 통한 spring 선언적 트랜잭션 설정
    spring-mybatis 모듈 설정
    테스트 데이터 초기화(datavasePopulator)

--- src/main/java/kr/or/ddit/ranger/dao/IRangerDao.java
+++ src/main/java/kr/or/ddit/ranger/dao/IRangerDao.java
@@ -1,6 +1,7 @@
 package kr.or.ddit.ranger.dao;
 
 import java.util.List;
+import java.util.Map;
 
 public interface IRangerDao {
 	
@@ -16,4 +17,38 @@ public interface IRangerDao {
 	 * @return
 	 */
 	String getRanger(int listIndex);
+
+	/**
+	 * db를 통한 레인저스 전체 조회
+	 * @return
+	 */
+	List<Map<String, String>> getRangerDb();
+	
+	/**
+	 * 레인저 아이디로 레인저 정보 조회
+	 * @param id
+	 * @return
+	 */
+	Map<String, String> getRanger(String id);
+	
+	/**
+	 * 신규 레인저 등록
+	 * @param map
+	 * @return
+	 */
+	int insertRanger(Map<String, String> map);
+	
+	/**
+	 * 레인저 삭제
+	 * @param id
+	 * @return
+	 */
+	int deleteRanger(String id);
+	
+	/**
+	 * 레인저 소속 삭제
+	 * @param id
+	 * @return
+	 */
+	int deleteRangerDept(String id);
 }
--- src/test/java/kr/or/ddit/ranger/dao/RangerDaoTest.java
+++ src/test/java/kr/or/ddit/ranger/dao/RangerDaoTest.java
@@ -2,10 +2,19 @@ package kr.or.ddit.ranger.dao;
 
 import static org.junit.Assert.*;
 
-import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -17,6 +26,17 @@ public class RangerDaoTest {
 	@Resource(name="rangerDao")
 	private IRangerDao rangerDao;
 	
+	@Resource(name="datasource")
+	private DataSource datasource;
+	
+	@Before
+	public void setup(){
+		ResourceDatabasePopulator rdp = new ResourceDatabasePopulator();
+		rdp.addScript(new ClassPathResource("kr/or/ddit/config/db/init.sql"));
+		rdp.setContinueOnError(false);
+		DatabasePopulatorUtils.execute(rdp, datasource);
+	}
+	
 	@Test
 	public void testGetRanger_overflowIndex() {
 		int index = 6;
@@ -26,4 +46,43 @@ public class RangerDaoTest {
 		assertEquals("james", ranger);
 	}
 
+	
+	/**
+	 * 전체 레인저스 조회(db)
+	 */
+	@Test
+	public void testGetRangerDb(){
+		List<Map<String,String>> rangerList = rangerDao.getRangerDb();
+		
+		assertEquals(4, rangerList.size());
+	}
+	
+	@Test
+	public void testGetRangerId(){
+		
+		Map<String, String> ranger = rangerDao.getRanger("brown");
+		
+		assertNotNull(ranger);
+		assertEquals("브라운", ranger.get("NAME"));
+	}
+	
+	@Test
+	public void testInsertRanger(){
+		Map<String, String> ranger = new HashMap<String,String>();
+		ranger.put("id", "ryon");
+		ranger.put("name", "라이언");
+		
+		int insertCnt = rangerDao.insertRanger(ranger);
+		
+		assertEquals(1, insertCnt);
+	}
+	
+	@Test
+	public void testDeleteRanger(){
+		rangerDao.deleteRangerDept("brown");
+		
+		int deleteCnt = rangerDao.deleteRanger("brown");
+		
+		assertEquals(1, deleteCnt);
+	}
 }
